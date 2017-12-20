2d38356f048a568b3fcf1e1bb5f19580d83686a8
burashka <burashka-lcme@yandex.ru>
2017-12-20 18:38:32 +0300

    Add redirect from HTTP to HTTPS Fixes #20

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const sync          = require('./routes/sync/sync');
 
 const app = express();
 
+function ensureSecure(req, res, next){
+	if(req.secure){
+		return next();
+	};
+	res.redirect('https://' + req.hostname + ":" + (process.env.HTTPS_PORT || 443) + req.url);
+}
+
+app.all('*', ensureSecure);
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
